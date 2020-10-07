78f073c292f8eab03b168386f50c2be364df3149
Elias93800 <71765411+Elias93800@users.noreply.github.com>
2020-10-07 20:43:06 +0200

    Update pyglet_frero.py

--- pyglet_frero.py
+++ pyglet_frero.py
@@ -1,7 +1,6 @@
 import pyglet
-from pyglet import shapes
 
-window = pyglet.window.Window(960,540)
+window = pyglet.window.Window(fullscreen = True)
 pyglet.gl.glClearColor(1,0.40,0.3, 1)
 batch = pyglet.graphics.Batch()
 
@@ -17,13 +16,20 @@ class Rectangle:
         self.b = b
         self.rectangle = pyglet.shapes.Rectangle(x_pos, y_pos, x, y, color=(r, g, b), batch=batch)
 
-barre1 = Rectangle(175, 0, 10, 200, 255, 22, 20 )
+barre1 = Rectangle(175, 0, 25, 300, 255, 255, 255)
+base1 = Rectangle(35, 0, 300, 30, 255, 255, 255)
 
-rectangle1 = Rectangle(50, 0, 275, 50, 93, 172, 255)
-rectangle2 = Rectangle(75, 50, 225, 45, 51, 141, 255)
-rectangle3 = Rectangle(100, 95, 175, 40, 245, 241, 11)
-rectangle4 = Rectangle(125, 135, 125, 35, 21,209, 125 ) 
-rectangle5 = Rectangle(150, 170, 75, 30, 223,209, 125 ) 
+barre2 = Rectangle(700, 0, 25, 300, 255, 255, 255)
+base2 = Rectangle(560, 0, 300, 30, 255, 255, 255)
+
+barre3 = Rectangle(1225, 0, 25, 300, 255, 255, 255)
+base3 = Rectangle(1085, 0, 300, 30, 255, 255, 255)
+
+rectangle1 = Rectangle(50, 30, 275, 50, 93, 172, 255)
+rectangle2 = Rectangle(75, 80, 225, 45, 51, 141, 255)
+rectangle3 = Rectangle(100, 125, 175, 40, 245, 241, 11)
+rectangle4 = Rectangle(125, 165, 125, 35, 21,209, 125) 
+rectangle5 = Rectangle(150, 200, 75, 30, 223,209, 125)
 
 
 @window.event
@@ -31,3 +37,4 @@ def on_draw():
     window.clear()
     batch.draw()
 pyglet.app.run()
+
