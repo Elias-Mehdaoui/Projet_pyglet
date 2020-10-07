3dd06b541e051dc2a297f4825ca30c3a436c2ae4
Erouthen <71765433+Erouthen@users.noreply.github.com>
2020-10-07 14:59:36 +0200

    Update pyglet_frero.py

--- pyglet_frero.py
+++ pyglet_frero.py
@@ -1,6 +1,8 @@
 import pyglet
+from pyglet import shapes
 
 window = pyglet.window.Window(960,540)
+pyglet.gl.glClearColor(1,0.40,0.3, 1)
 batch = pyglet.graphics.Batch()
 
 class Rectangle:
@@ -15,18 +17,17 @@ class Rectangle:
         self.b = b
         self.rectangle = pyglet.shapes.Rectangle(x_pos, y_pos, x, y, color=(r, g, b), batch=batch)
 
+barre1 = Rectangle(175, 0, 10, 200, 255, 22, 20 )
 
+rectangle1 = Rectangle(50, 0, 275, 50, 93, 172, 255)
+rectangle2 = Rectangle(75, 50, 225, 45, 51, 141, 255)
+rectangle3 = Rectangle(100, 95, 175, 40, 245, 241, 11)
+rectangle4 = Rectangle(125, 135, 125, 35, 21,209, 125 ) 
+rectangle5 = Rectangle(150, 170, 75, 30, 223,209, 125 ) 
 
-
-rectangle1 = Rectangle(50, 0, 275, 50, 255, 22, 20)
-rectangle2 = Rectangle(75, 50, 225, 50, 18, 59, 132)
-rectangle3 = Rectangle(87.5, 75, 175, 50, 245, 241, 11)
-
-    
 
 @window.event
 def on_draw():
     window.clear()
     batch.draw()
-    
 pyglet.app.run()
