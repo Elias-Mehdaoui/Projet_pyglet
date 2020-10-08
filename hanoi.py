0a1f1d07e1ca5337231a1dc41d0d153bb20367d8
Erouthen <jounayd.el.yaouti@lasallesaintdenis.com>
2020-10-08 14:58:53 +0200

    Finalisation de l'affichage de la tour de Hanoi

--- hanoi.py
+++ hanoi.py
@@ -1,7 +1,7 @@
 import pyglet
 
 window = pyglet.window.Window(fullscreen = True)
-pyglet.gl.glClearColor(1,0.40,0.3, 1)
+pyglet.gl.glClearColor(0.28,0.407,0.372, 1)
 batch = pyglet.graphics.Batch()
 
 class Rectangle:
@@ -25,13 +25,18 @@ base2 = Rectangle(560, 0, 300, 30, 255, 255, 255)
 barre3 = Rectangle(1225, 0, 25, 300, 255, 255, 255)
 base3 = Rectangle(1085, 0, 300, 30, 255, 255, 255)
 
-rectangle1 = Rectangle(50, 30, 275, 50, 93, 172, 255)
-rectangle2 = Rectangle(75, 80, 225, 45, 51, 141, 255)
-rectangle3 = Rectangle(100, 125, 175, 40, 245, 241, 11)
-rectangle4 = Rectangle(125, 165, 125, 35, 21,209, 125) 
-rectangle5 = Rectangle(150, 200, 75, 30, 223,209, 125)
+rectangle1 = Rectangle(50, 30, 275, 50, 0, 170, 255)
+rectangle2 = Rectangle(75, 80, 225, 45, 0, 212, 255)
+rectangle3 = Rectangle(100, 125, 175, 40, 0, 255, 228)
+rectangle4 = Rectangle(125, 165, 125, 35, 21,255, 189) 
+rectangle5 = Rectangle(150, 200, 75, 30, 0,255, 178)
 
-
+"""
+Nous avond donc crée une classe Rectangle afin de modeliser nos differents
+rectangle de notre mini-jeu. Ainsi, on a pu optimiser notre rectangle en lui
+donnat sa position (x, y) ainsi que sa taille sur X et sur Y. Et enfin nous 
+avons informer notre code de la couleur.
+"""
 @window.event
 def on_draw():
     window.clear()
