91aea1501695cb416d7bb50004695198af9ee4a1
Erouthen <71765433+Erouthen@users.noreply.github.com>
2020-10-31 15:26:08 +0100

    Update pyglet_frero.py

--- pyglet_frero.py
+++ pyglet_frero.py
@@ -1,12 +1,30 @@
+"""
+                                TOUR DE HANNOI
+Le projet consiste a résoudre le casse tête de "La tour de Hannoi" et de crée une interface
+graphique ou le joueur peut interragir avec les differents objets de la fennêtre.
+"""
 import pyglet
 
 window = pyglet.window.Window(720, 380, resizable = True)
 pyglet.gl.glClearColor(1,0.40,0.3, 1)
 batch = pyglet.graphics.Batch()
-
+"""
+Pour ce faire nous créons une fenetre en deffinissant l'arriere plans c'est-à-dire la couleur 
+via le systeme RGB ansi que l'opacité de la fenetre
+Nous rentrons aussi dans les paramètre de la fênetre la resolution c'est-à-dire la taille de notre fenétre
+(ici 720*380 )
+"""
 class Rectangle:
-    
+    """
+    Afin d'eviter la repetitons de notre code, nous créons une classe qui vas nous permettre de crée un ensemble de
+    rectangle. Notre code est optimiser au maximum. On peut y retrouver plusieurs methode
+    """
     def __init__(self, x_pos, y_pos, x, y, r, g, b):
+        """
+        Tout d'abord nous initialisons notre objet (rectangle) en informant le programme des caractéristique primaire
+        du rectangle c'est-à-dire les cordonné de son point le plus en bas gauche(x, y) ansi que sa taille sur (x, y) mais aussi sa couleur, toujours en suivant
+        le systéme RGB.
+        """
         self.x = x
         self.y = y
         self.x_pos = x_pos
@@ -42,8 +60,12 @@ base2 = Rectangle(560, 0, 300, 30, 255, 255, 255)
 barre3 = Rectangle(1225, 0, 25, 300, 255, 255, 255)
 base3 = Rectangle(1085, 0, 300, 30, 255, 255, 255)
 
+"""
+Nous créons donc les cinq rectangle que nous allons afficher ainsi que les barres et les 
+bases de chaque tours.
+"""
 
-rectangle5 = Rectangle(50, 30, 275, 50, 93, 172, 255)
+rectangle5 = Rectangle(50, 30, 275, 50, 93, 172, 255)# rectangle = Rectangle (x_pos, y_pos, x, y, r, g, b )
 rectangle4 = Rectangle(75, 80, 225, 45, 51, 141, 255)
 rectangle3 = Rectangle(100, 125, 175, 40, 245, 241, 11)
 rectangle2 = Rectangle(125, 165, 125, 35, 21,209, 125) 
@@ -75,7 +97,11 @@ def on_mouse_press(x, y, button, modifiers):
         
 
 
+"""
+cette dernière partie permet de lancer le programmes d'affichage, et lance la fenètre.*
+Le code ici nettoi tout d'abord la fenetre, puis dessine le contenue et les objets coder précedement
 
+"""
 @window.event
 def on_draw():
     window.clear()
